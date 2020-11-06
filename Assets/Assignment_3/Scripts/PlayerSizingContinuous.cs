c266ef8628b96b8406e210b34be911ef91b2e04b
AJ Atkins <andrew.justus.atkins@gmail.com>
2020-11-06 14:41:47 -0600

    Continuous size changing is mostly complete.

---
+++ Assets/Assignment_3/Scripts/PlayerSizingContinuous.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSizingContinuous : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject m_PlayerController;
+
+    public float playerSizeIncrement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If the joystick is going up, increase player size
+        if(Input.GetAxis("Oculus_CrossPlatform_SecondaryThumbstickVertical") > 0)
+        {
+            gameObject.transform.localScale += playerSizeIncrement * Vector3.one;
+        }
+        // If the joystick is going down, decrease player size
+        else if (Input.GetAxis("Oculus_CrossPlatform_SecondaryThumbstickVertical") < 0)
+        {
+            gameObject.transform.localScale -= playerSizeIncrement * Vector3.one;
+        }
+
+        //gameObject.transform.position = gameObject.transform.position - m_PlayerController.transform.position;
+    }
+}
---
+++ Assets/Assignment_3/Scripts/TeleportDestinationSizing.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportDestinationSizing : MonoBehaviour
+{
+    [SerializeField]
+    GameObject m_PlayerParent;
+
+    private Vector3 playerSizeAtStart;
+    private Vector3 teleportSizeAtStart;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerSizeAtStart = m_PlayerParent.transform.localScale;
+        teleportSizeAtStart = gameObject.transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerSize = m_PlayerParent.transform.localScale;
+        //Vector3 playerScale = new Vector3(playerSize.x / playerSizeAtStart.x, playerSize.y / playerSizeAtStart.y, playerSize.z / playerSizeAtStart.z);
+        float playerScale = playerSize.x / playerSizeAtStart.x;
+        gameObject.transform.localScale = teleportSizeAtStart * playerScale;
+    }
+}
