a89cbe7fe67717e5a17b8c78e491cf02d4d265cd
AJ Atkins <andrew.justus.atkins@gmail.com>
2020-11-08 18:20:43 -0600

    Player can now change the size of objects

---
+++ Assets/Assignment_3/Scripts/GrabableSizing.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabableSizing : MonoBehaviour
+{
+    [SerializeField]
+    OVRGrabbable m_Grabbable;
+
+    public Vector3 maxObjectSize;
+    public Vector3 minObjectSize;
+    public float objectSizeIncrement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(m_Grabbable.isGrabbed)
+        {
+            // If the joystick is going up, increase player size
+            if (Input.GetAxis("Oculus_CrossPlatform_SecondaryThumbstickVertical") > 0)
+            {
+                if (m_Grabbable.gameObject.transform.localScale.y < maxObjectSize.y)
+                {
+                    m_Grabbable.gameObject.transform.localScale += objectSizeIncrement * Vector3.one;
+                }
+            }
+            // If the joystick is going down, decrease player size
+            else if (Input.GetAxis("Oculus_CrossPlatform_SecondaryThumbstickVertical") < 0)
+            {
+                if (m_Grabbable.gameObject.transform.localScale.y > minObjectSize.y)
+                {
+                    m_Grabbable.gameObject.transform.localScale -= objectSizeIncrement * Vector3.one;
+                }
+            }
+        }
+    }
+}
--- Assets/Assignment_3/Scripts/Player/PlayerSizingContinuous.cs
+++ Assets/Assignment_3/Scripts/Player/PlayerSizingContinuous.cs
@@ -2,14 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.VR;
+using UnityEngine.XR;
 
 public class PlayerSizingContinuous : MonoBehaviour
 {
     [SerializeField]
     public GameObject m_PlayerController;
-
-    [SerializeField]
-    public GameObject child;
 
     [SerializeField]
     public GameObject m_CenterEyeAnchor;
@@ -27,22 +25,26 @@ public class PlayerSizingContinuous : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        XRDevice.SetTrackingSpaceType(UnityEngine.XR.TrackingSpaceType.RoomScale);
     }
 
     // Update is called once per frame
     void Update()
     {
+        /* Reposition the player objects to be where the head is */
+        // detach child
+        m_OVRCameraRig.transform.parent = null;
+        // change position
+        transform.position = new Vector3(m_CenterEyeAnchor.transform.position.x, transform.position.y, m_CenterEyeAnchor.transform.position.z);
+        // reattach child
+        m_OVRCameraRig.transform.parent = m_PlayerController.transform;
+        
+        /* Handle player resizing */
         // If the joystick is going up, increase player size
         if (Input.GetAxis("Oculus_CrossPlatform_SecondaryThumbstickVertical") > 0)
         {
             if(gameObject.transform.localScale.y < maxPlayerSize.y)
             {
-                // detach child
-                m_OVRCameraRig.transform.parent = null;
-                // change position
-                transform.position = new Vector3(m_CenterEyeAnchor.transform.position.x, transform.position.y, m_CenterEyeAnchor.transform.position.z);
-                // reattach child
-                m_OVRCameraRig.transform.parent = m_PlayerController.transform;
                 gameObject.transform.localScale += playerSizeIncrement * Vector3.one;
             }
         }
@@ -51,12 +53,6 @@ public class PlayerSizingContinuous : MonoBehaviour
         {
             if (gameObject.transform.localScale.y > minPlayerSize.y)
             {
-                // detach child
-                m_OVRCameraRig.transform.parent = null;
-                // change position
-                transform.position = new Vector3(m_CenterEyeAnchor.transform.position.x, transform.position.y, m_CenterEyeAnchor.transform.position.z);
-                // reattach child
-                m_OVRCameraRig.transform.parent = m_PlayerController.transform;
                 gameObject.transform.localScale -= playerSizeIncrement * Vector3.one;
             }
         }
