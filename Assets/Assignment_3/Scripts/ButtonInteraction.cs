f7d27b8d3502edc7af7a213ab09b492904384561
amojena <am3238@cornell.edu>
2020-11-17 11:01:22 -0500

    Puzzle design in the works, bugs to fix

---
+++ Assets/Assignment_3/Scripts/ButtonInteraction.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ButtonInteraction : MonoBehaviour
+{
+    
+    [System.Serializable]
+    public class ButtonEvent : UnityEvent { }
+
+    [SerializeField]
+    GameObject midWallParent;
+
+    bool moveWalls;
+
+    public float pressLength;
+    public bool pressed;
+    public ButtonEvent downEvent;
+
+    Vector3 startPos;
+    Rigidbody rb;
+
+
+    void Start()
+    {
+        startPos = transform.position;
+        rb = GetComponent<Rigidbody>();
+        moveWalls = false;
+    }
+
+    void Update()
+    {
+        // If our distance is greater than what we specified as a press
+        // set it to our max distance and register a press if we haven't already
+        float distance = Mathf.Abs(transform.position.z - startPos.z);
+        if (distance >= pressLength)
+        {
+            // Prevent the button from going past the pressLength
+            transform.position = new Vector3(transform.position.x, startPos.y - pressLength, transform.position.z);
+            if (!pressed)
+            {
+                pressed = true;
+                moveWalls = true;
+
+            }
+        } else
+        {
+            // If we aren't all the way down, reset our press
+            pressed = false;
+        }
+        // Prevent button from springing back up past its original position
+        if (transform.position.z > startPos.z)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, startPos.z);
+        }
+
+        if (moveWalls)
+            midWallParent.transform.position = Vector3.Lerp(midWallParent.transform.position, new Vector3(0,10,0), Time.deltaTime * 1.2f);
+
+    }
+}
---
+++ Assets/Assignment_3/Scripts/HandScanner.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HandScanner : MonoBehaviour
+{
+    [SerializeField]
+    GameObject wallParent, parent;
+
+    [SerializeField]
+    TextMeshPro playerSizeText;
+
+    bool moveWalls;
+
+    [SerializeField]
+    float maxPlayerScale = 1f;
+
+    PlayerSizingContinuous player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").GetComponent<PlayerSizingContinuous>();
+        moveWalls = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(moveWalls) 
+        {
+            wallParent.transform.position = Vector3.Lerp(wallParent.transform.position, new Vector3(0,5,0), Time.deltaTime * .3f);
+        }
+
+        playerSizeText.text = player.scaleFactor.ToString("F2") + moveWalls.ToString();
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (player.scaleFactor <= maxPlayerScale) {
+            MeshRenderer[] meshRenderers = parent.GetComponentsInChildren<MeshRenderer>();
+            foreach(MeshRenderer mr in meshRenderers)
+                mr.enabled = false;
+            moveWalls = true;
+        }
+    }
+}
