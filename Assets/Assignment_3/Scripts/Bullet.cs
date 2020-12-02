7466a79e56fcc0ba6cfc02777ddc9d4d18f5c7e9
AJ Atkins <andrew.justus.atkins@gmail.com>
2020-12-02 13:57:26 -0600

    Bullets now are destroyed after a bit of time. Players should be able to see bullets

---
+++ Assets/Assignment_3/Scripts/Bullet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float lifeSpan = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, lifeSpan);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
