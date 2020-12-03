e98bcb482653576e8bb9eb18a8573a54ee00b1cc
noah-rush <noahjrush@gmail.com>
2020-12-02 22:31:08 -0500

    tiny tunnel, candles and match

---
+++ Assets/Match.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Match : MonoBehaviour
+{
+    // Start is called before the first frame update
+    bool lit = true;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public bool isLit(){
+    	return lit;
+    }
+}
