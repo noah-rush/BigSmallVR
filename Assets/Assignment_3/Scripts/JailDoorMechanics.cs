040cde43808f371f18f230f54bc49602dc5135fe
amojena <am3238@cornell.edu>
2020-12-04 17:41:43 -0500

    Trying to save my scripts/prefabs for merge

---
+++ Assets/Assignment_3/Scripts/JailDoorMechanics.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JailDoorMechanics : MonoBehaviour
+{
+    GameObject jailDoor;
+    Vector3 startPos, jailDoorStartPos;
+    float heightDiff = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        jailDoor = GameObject.Find("Jail Door");
+
+        startPos = transform.position;
+        jailDoorStartPos = jailDoor.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        heightDiff = transform.position.y - startPos.y;
+        jailDoor.transform.position = jailDoorStartPos + new Vector3(0, heightDiff, 0);
+    }
+}
