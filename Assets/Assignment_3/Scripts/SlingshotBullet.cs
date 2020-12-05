85181de60a365565cc42ff28e58de0dd87255f0d
AJ Atkins <andrew.justus.atkins@gmail.com>
2020-12-05 17:45:05 -0600

    Slingshot is working. Need to add linerenderer

---
+++ Assets/Assignment_3/Scripts/SlingshotBullet.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlingshotBullet : MonoBehaviour
+{
+    public Transform playerHand;
+    public Transform slingshotTip;
+    public GrabbableSizing slingshot;
+    public OVRGrabber grabbingPlayer;
+
+    Vector3 originalScale;
+    bool launched;
+    int launchForce = 30;
+    float maxStringPulled = 2.5f;
+
+    float lifeSpan = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        launched = false;
+        originalScale = transform.localScale;
+        transform.localScale = transform.localScale * slingshot.scaleFactor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OVRInput.SetControllerVibration(1, 0, OVRInput.Controller.LTouch);
+        OVRInput.SetControllerVibration(1, 0, OVRInput.Controller.RTouch);
+        if (!launched)
+        {
+            if (playerHand && slingshot && grabbingPlayer)
+            {
+                // if the player lets go, launch the ball
+                if ((grabbingPlayer.m_controller == OVRInput.Controller.LTouch && OVRInput.Get(OVRInput.RawAxis1D.RIndexTrigger) <= 0.05f // players holding in left hand lets go of ball with right
+                || grabbingPlayer.m_controller == OVRInput.Controller.RTouch && OVRInput.Get(OVRInput.RawAxis1D.LIndexTrigger) <= 0.05f))
+                {
+                    launched = true;
+                    Rigidbody rb = GetComponent<Rigidbody>();
+                    rb.AddForce((slingshotTip.position - transform.position) * launchForce, ForceMode.Impulse);
+                    Destroy(gameObject, lifeSpan); 
+
+                }
+                else // else move the ball with the players hand
+                {
+                    // move and scale
+                    transform.position = playerHand.position;
+                    transform.localScale = originalScale * slingshot.scaleFactor;
+                    // vibrate if the ball is far away from slingshot tip
+                    float stringPulled = (slingshotTip.position - transform.position).magnitude / slingshot.scaleFactor;
+                    if(stringPulled >= maxStringPulled)
+                    {
+                        // vibrate
+                        if(grabbingPlayer.m_controller == OVRInput.Controller.LTouch)
+                        {
+                            // vibrate left controller
+                            OVRInput.SetControllerVibration(1, Mathf.Min(0.1f * stringPulled, 1f), OVRInput.Controller.RTouch);
+
+                        } else if(grabbingPlayer.m_controller == OVRInput.Controller.RTouch)
+                        {
+                            // vibrate right controller
+                            OVRInput.SetControllerVibration(1, Mathf.Min(0.1f * stringPulled, 1f), OVRInput.Controller.LTouch);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- Assets/Assignment_3/Scripts/SlingshotShooter.cs
+++ Assets/Assignment_3/Scripts/SlingshotShooter.cs
@@ -8,9 +8,6 @@ using Normal.Realtime;
 
 public class SlingshotShooter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    // [SerializeField]
-    // GameObject bullet;
 
     [SerializeField]
     Transform crossbowTip;
@@ -18,55 +15,39 @@ public class SlingshotShooter : MonoBehaviour
     private OVRGrabber grabbingPlayer;
     private DistanceGrabbable crossbow;
 
+    GameObject currBullet;
+
     bool needsReload = false;
 
 
     void Start()
     {
-        crossbow = gameObject.GetComponent<DistanceGrabbable>();
-        _realtime = GetComponent<Realtime>();
+        crossbow = GetComponentInParent<DistanceGrabbable>();
+        currBullet = null;
+        _realtime = GetComponentInParent<Realtime>();
     }
 
-    private bool GetTriggerPulled()
+    void OnTriggerStay(Collider collision)
     {
-        bool pulledTrigger = false;
-        if (grabbingPlayer.m_controller == OVRInput.Controller.LTouch && OVRInput.Get(OVRInput.RawButton.LIndexTrigger)
-            || grabbingPlayer.m_controller == OVRInput.Controller.RTouch && OVRInput.Get(OVRInput.RawButton.RIndexTrigger))
+        if (currBullet == null 
+            && ((collision.gameObject.tag == "LeftHand" 
+            && OVRInput.Get(OVRInput.RawButton.LIndexTrigger))
+            || (collision.gameObject.tag == "RightHand"
+            && OVRInput.Get(OVRInput.RawButton.RIndexTrigger))))
         {
-            pulledTrigger = true;
-        }
-
-        return pulledTrigger;
-    }
-
-    private void Reload()
-    {
-        // check if the player has let go of the trigger
-        if (needsReload)
-        {
-            if (grabbingPlayer.m_controller == OVRInput.Controller.LTouch && OVRInput.Get(OVRInput.RawAxis1D.LIndexTrigger) == 0f
-                || grabbingPlayer.m_controller == OVRInput.Controller.RTouch && OVRInput.Get(OVRInput.RawAxis1D.RIndexTrigger) == 0f)
-            {
-                needsReload = false;
-            }
-        }
-    }
-
-    private void Shoot()
-    {
-        // /   / force a reload 
-        needsReload = true;
-        // fire bullet
-        GameObject currBullet = Realtime.Instantiate("Bullet",                 // Prefab name
-                             position: crossbowTip.position,          // Start 1 meter in the air
+            currBullet = Realtime.Instantiate("SlingshotBullet",                 // Prefab name
+                             position: collision.transform.position,          // Start 1 meter in the air
                              rotation: crossbowTip.rotation, // No rotation
                              ownedByClient: false,   // Make sure the RealtimeView on this prefab is NOT owned by this client
                              preventOwnershipTakeover: false,                // DO NOT prevent other clients from calling RequestOwnership() on the root RealtimeView.
                              useInstance: _realtime);           // Use the instance of Realtime that fired the didConnectToRoom event.
 
-        float crossbowScaleFactor = gameObject.GetComponent<GrabbableSizing>().scaleFactor;
-        currBullet.transform.localScale = currBullet.transform.localScale * crossbowScaleFactor;
-        currBullet.GetComponent<Rigidbody>().AddForce((currBullet.transform.position - transform.position) * (40 /* * crossbowScaleFactor*/), ForceMode.Impulse);
+            SlingshotBullet currBulletScript = currBullet.GetComponent<SlingshotBullet>();
+            currBulletScript.slingshot = GetComponentInParent<GrabbableSizing>();
+            currBulletScript.playerHand = collision.transform;
+            currBulletScript.grabbingPlayer = grabbingPlayer;
+            currBulletScript.slingshotTip = crossbowTip;
+        }
     }
 
     // Update is called once per frame
@@ -74,18 +55,17 @@ public class SlingshotShooter : MonoBehaviour
     {
         /* Handle firing the gun */
         grabbingPlayer = crossbow.grabbedBy;
-        if (!grabbingPlayer) return;
-
-        // check if the player has let go of the trigger
-        Reload();
-
-        // check if the player is pulling the trigger more than 50% of the way
-        bool pulledTrigger = GetTriggerPulled();
-
-        // if the player pulled the trigger and has let go of the trigger since the last time, shoot
-        if (pulledTrigger && !needsReload)
+        if (!grabbingPlayer)
         {
-            Shoot();
+            return;
+        }
+
+        // Check if player let go of the ball
+        if (currBullet != null
+            && (grabbingPlayer.m_controller == OVRInput.Controller.LTouch && OVRInput.Get(OVRInput.RawAxis1D.RIndexTrigger) <= 0.05f // players holding in left hand lets go of ball with right
+            || grabbingPlayer.m_controller == OVRInput.Controller.RTouch && OVRInput.Get(OVRInput.RawAxis1D.LIndexTrigger) <= 0.05f))
+        {
+            currBullet = null;
         }
     }
 }
