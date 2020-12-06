6162425d2f95d36d71cf6dfad2894267d9f37750
amojena <am3238@cornell.edu>
2020-12-06 16:24:52 -0500

    Organized assets folder and added controller instruction when holding slingshot

---
+++ Assets/Assignment_3/Scripts/ControllerBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerBehavior : MonoBehaviour
+{
+    float timer = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.activeSelf) timer += Time.deltaTime;
+
+        if (timer > 5f)
+        {
+            gameObject.SetActive(false);
+            timer = 0f;
+        }
+
+
+
+    }
+}
---
+++ Assets/Assignment_3/Scripts/ControllerInstructions.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ControllerInstructions : MonoBehaviour
+{
+    [SerializeField]
+    GameObject controllerInHand, controllerInOtherHand;
+
+    OVRGrabbable slingshot;
+    bool slingshotHelp = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slingshot = GameObject.Find("Slingshot").GetComponent<OVRGrabbable>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (slingshot.isGrabbed)
+        {
+            //slingText.text = slingshot.grabbedBy
+            if (slingshot.grabbedBy.name.Equals(gameObject.name) && slingshotHelp)
+            {
+                controllerInOtherHand.SetActive(true);
+                slingshotHelp = false;
+            }
+        }
+    }
+
+
+
+}
