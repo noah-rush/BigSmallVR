e579afa72eeeead75240bb450e6f2d1c0634ab53
noah-rush <noahjrush@gmail.com>
2020-12-06 17:26:59 -0500

    fixing script org bug

--- Assets/Assignment_3/Scripts/CodeKey.cs
+++ Assets/Assignment_3/Scripts/CodeKey.cs
@@ -6,7 +6,7 @@ public class CodeKey : MonoBehaviour
 {
 
     public float endstop;//how far down you want the button to be pressed before it triggers
-    public bool Pressed;
+    public bool Pressed = false;
 
     private Transform Location;
     private Vector3 StartPos;
@@ -21,7 +21,7 @@ public class CodeKey : MonoBehaviour
     void Update()
     {
 
-        if (Mathf.Abs(Location.position.z - StartPos.z) > endstop ) //check to see if the button has been pressed all the way down
+        if (Mathf.Abs(Location.position.z - StartPos.z) > endstop && !Pressed ) //check to see if the button has been pressed all the way down
         {
             // Location.position = new Vector3(Location.position.x, Location.position.y, endstop + StartPos.z);
             GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
@@ -46,12 +46,14 @@ public class CodeKey : MonoBehaviour
     //     }
     // }
     IEnumerator buttonReset(){
+        yield return new WaitForSeconds(.4f);
          for (float ft = 0f; ft <= 1f; ft += 0.1f) 
         {
             Location.position = Vector3.Lerp(endPos, StartPos, ft);
 
             yield return new WaitForSeconds(.1f);
         }
+        Pressed = false;
     }
 
 
