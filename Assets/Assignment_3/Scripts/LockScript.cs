c92921f306644e64a9f635ff23da7cfbf45948a6
AJ Atkins <andrew.justus.atkins@gmail.com>
2020-12-07 15:51:23 -0600

    Working on opening jail cell door with key

--- Assets/Assignment_3/Scripts/LockScript.cs
+++ Assets/Assignment_3/Scripts/LockScript.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public class LockScript : MonoBehaviour
 {
+    GameObject jailDoor;
+    Vector3 jailDoorStartPos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        jailDoor = GameObject.Find("Jail Door");
+        jailDoorStartPos = jailDoor.transform.position;
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "key")
+        {
+            //Rigidbody rb = jailDoor.GetComponent<Rigidbody>();
+            //rb.isKinematic = true;
+            jailDoor.transform.position = jailDoorStartPos + new Vector3(0, 8, 0);
+        }
+    }
     // Update is called once per frame
     void Update()
     {
