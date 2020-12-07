bafc1370665c2f3b14c3928defd954062d7a779f
noah-rush <noahjrush@gmail.com>
2020-12-07 17:19:56 -0500

    update

--- Assets/Assignment_3/Scripts/CodeSync.cs
+++ Assets/Assignment_3/Scripts/CodeSync.cs
@@ -13,37 +13,8 @@ public class CodeSync : RealtimeComponent<CodeSyncModel>
         _codeBox = GetComponent<CodeBox>();
     }
 
-    protected override void OnRealtimeModelReplaced(CodeSyncModel previousModel, CodeSyncModel currentModel)
-    {
-        if (previousModel != null)
-        {
-            // Unregister from events
-            previousModel.activatedDidChange -= activatedDidChange;
-
-        }
-
-        if (currentModel != null)
-        {
-            // If this is a model that has no data set on it, populate it with the current mesh renderer color.
-            if (currentModel.isFreshModel)
-                currentModel.activated = _codeBox.isReady();
-
-
-            // Update the mesh render to match the new model
-            // UpdateMatchLit();
-            // UpdateActivation();
-
-
-            // Register for events so we'll know if the color changes later
-            currentModel.activatedDidChange += activatedDidChange;
-
-        }
-    }
-
-    private void activatedDidChange(CodeSyncModel model, bool value)
-    {
+    void Update(){
         UpdateActivation();
-
     }
   
     private void UpdateActivation()
