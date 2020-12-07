74bb42858b2453bf2559f13de8531666560eb7aa
AJ Atkins <andrew.justus.atkins@gmail.com>
2020-12-07 12:27:13 -0600

    Added button to open dog door

---
+++ Assets/Assignment_3/Scripts/DogDoorButton.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DogDoorButton : MonoBehaviour
+{
+    public HingeJoint dogDoorHinge;
+
+    public float endstop;//how far down you want the button to be pressed before it triggers
+    public bool Pressed = false;
+
+    private Transform Location;
+    private Vector3 StartPos;
+    private Vector3 endPos;
+    private string number;
+    CodeBox codeBox;
+    TMP_Text entryPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Location = transform;
+        StartPos = Location.position;
+        //number = transform.GetChild(0).GetComponent<TMP_Text>().text;
+        //codeBox = transform.parent.gameObject.GetComponent<CodeBox>();
+        Physics.IgnoreCollision(codeBox.ColorPlane.GetComponent<BoxCollider>(), GetComponent<BoxCollider>());
+    }
+    void Update()
+    {
+
+        if (Mathf.Abs(Location.position.z - StartPos.z) > endstop && !Pressed) //check to see if the button has been pressed all the way down
+        {
+            // Location.position = new Vector3(Location.position.x, Location.position.y, endstop + StartPos.z);
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+            Pressed = true;//update pressed
+            //codeBox.addToKeyCode(number);
+            dogDoorHinge.useSpring = true;
+            endPos = Location.position;
+            StartCoroutine("buttonReset");
+
+
+
+        }
+        else
+        {
+
+        }
+
+
+    }
+    IEnumerator buttonReset()
+    {
+        yield return new WaitForSeconds(.4f);
+        for (float ft = 0f; ft <= 1f; ft += 0.01f)
+        {
+            Location.position = Vector3.Lerp(endPos, StartPos, ft);
+            yield return new WaitForSeconds(.005f);
+        }
+        Pressed = false;
+        GetComponent<Rigidbody>().constraints &= ~RigidbodyConstraints.FreezePositionZ; //Remove Y movement constraint.
+
+    }
+
+
+}
