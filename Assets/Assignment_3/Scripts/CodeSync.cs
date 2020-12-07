09276a64ef9417b23ebb31fd08665c6e196b2a09
noah-rush <noahjrush@gmail.com>
2020-12-07 15:59:52 -0500

    code box

--- Assets/Assignment_3/Scripts/CodeSync.cs
+++ Assets/Assignment_3/Scripts/CodeSync.cs
@@ -19,7 +19,6 @@ public class CodeSync : RealtimeComponent<CodeSyncModel>
         {
             // Unregister from events
             previousModel.activatedDidChange -= activatedDidChange;
-            previousModel.keycodeDidChange -= keycodeDidChange;
 
         }
 
@@ -28,17 +27,14 @@ public class CodeSync : RealtimeComponent<CodeSyncModel>
             // If this is a model that has no data set on it, populate it with the current mesh renderer color.
             if (currentModel.isFreshModel)
                 currentModel.activated = _codeBox.isActive();
-            currentModel.keycode = _codeBox.getKeycode();
 
 
             // Update the mesh render to match the new model
             // UpdateMatchLit();
             UpdateActivation();
-            UpdateKeycode();
 
 
             // Register for events so we'll know if the color changes later
-            currentModel.keycodeDidChange += keycodeDidChange;
             currentModel.activatedDidChange += activatedDidChange;
 
         }
@@ -49,40 +45,24 @@ public class CodeSync : RealtimeComponent<CodeSyncModel>
         UpdateActivation();
 
     }
-    private void keycodeDidChange(CodeSyncModel model, string value)
-    {
-        // Update the mesh renderer
-        UpdateKeycode();
-    }
+  
     private void UpdateActivation()
     {
         // Get the color from the model and set it on the mesh renderer.
         if(model.activated)
         {
             _codeBox.activate();
-
         }
         else
         {
             _codeBox.deactivate();
 
         }
-        // _codeBox.activateFromModel(model.activated);
     }
-    private void UpdateKeycode()
-    {
-        // Get the color from the model and set it on the mesh renderer.
-        _codeBox.setKeycode(model.keycode);
-    }
-
+ 
     public void SetActivation(bool value)
     {
         model.activated = value;
     }
-    public void SetKeycode(string keycode)
-    {
-        // Set the color on the model
-        // This will fire the colorChanged event on the model, which will update the renderer for both the local player and all remote players.
-        model.keycode = keycode;
-    }
+ 
 }
